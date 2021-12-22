b101314af3305b0cb59816a8acf46c07fd3fcd49
trungnghia-be <trungnghia161201@gmail.com>
2021-12-22 09:47:15 +0700

    Edit_2

--- LMS_DashBoard/script.js
+++ LMS_DashBoard/script.js
@@ -1,6 +1,5 @@
 let sidebar = document.querySelector(".sidebar");
 let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
 
 closeBtn.addEventListener("click", ()=>{
   sidebar.classList.toggle("open");
